66f6990a5952a57b7c4875f7e959fc9521945ed5
mozbeel <matthiasleeb4@gmail.com>
2025-07-24 11:58:23 +0200

    Hopefully fixed android issue

--- build.zig
+++ build.zig
@@ -994,7 +994,7 @@ pub fn build(b: *std.Build) !void {
     if (android) {
         try sdl_c_flags.appendSlice(&.{
             "-D_POSIX_C_SOURCE=200809L", // Add this line!
-        // or try "-D_GNU_SOURCE" if the above doesn't work
+            "_GNU_SOURCE=1",
         });
         sdl_mod.addCSourceFiles(.{
             .flags = sdl_c_flags.slice(),
